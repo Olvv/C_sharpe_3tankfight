1f7ab6cea5e54a94e8e4eaa96828019941b1d0ec
tianceli <tianceli@gmail.com>
2025-07-01 23:46:36 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_sharpe_3tankfight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            //this.StartPosition= FormStartPosition.Manual;
+            //this.Location = new Point(100,100);
+
+            //GDI Graphics Device Interface
+        }
+
+
+    }
+}
